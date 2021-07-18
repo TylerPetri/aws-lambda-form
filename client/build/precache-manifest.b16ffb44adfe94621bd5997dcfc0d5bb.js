fc1e3e30bd249542dee91eb3d60c3bd5fc1f4089
Tyler Petri <tyler_1701@hotmail.com>
2021-07-17 23:48:35 -0400

    readme

---
+++ client/build/precache-manifest.b16ffb44adfe94621bd5997dcfc0d5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6daf3e4a14bb4d38bc526de1d8669a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f1d156ae65ea8bae06",
+    "url": "/static/css/main.87461779.chunk.css"
+  },
+  {
+    "revision": "90360f2a9a618486d313",
+    "url": "/static/js/2.04b3f908.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.04b3f908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f1d156ae65ea8bae06",
+    "url": "/static/js/main.50e8dc6f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d26d836e97df5275a06aad61080991b4",
+    "url": "/static/media/spinner.d26d836e.gif"
+  }
+]);
